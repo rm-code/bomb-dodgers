76d285c9ff0ed90efea7d95397ece3ccdecb863f
Robert Machmer <robert.machmer@gmail.com>
2014-07-30 13:35:51 +0200

    Add explosion sprites.
    Explosions get assigned a certain sprite based on their direction. The
    origin will branch out in all four directions. North and South sprites
    get a vertical explosion sprite, whereas east and west get a horizontal
    one.
    
    The end of each explosion is determined by the detonate signal's
    strength. If it is zero, we know that it's the last tile in to draw.

--- src/game/Tile.lua
+++ src/game/Tile.lua
@@ -70,7 +70,33 @@ function Tile.new()
                 return;
             end
         else
-            self:addContent(Explosion.new());
+            if detonate.direction == 'all' then
+                self:addContent(Explosion.new('origin'));
+            elseif detonate.direction == 'east' then
+                if detonate.strength == 0 then
+                    self:addContent(Explosion.new('endeast'));
+                else
+                    self:addContent(Explosion.new('horizontal'));
+                end
+            elseif detonate.direction == 'west' then
+                if detonate.strength == 0 then
+                    self:addContent(Explosion.new('endwest'));
+                else
+                    self:addContent(Explosion.new('horizontal'));
+                end
+            elseif detonate.direction == 'north' then
+                if detonate.strength == 0 then
+                    self:addContent(Explosion.new('endnorth'));
+                else
+                    self:addContent(Explosion.new('vertical'));
+                end
+            elseif detonate.direction == 'south' then
+                if detonate.strength == 0 then
+                    self:addContent(Explosion.new('endsouth'));
+                else
+                    self:addContent(Explosion.new('vertical'));
+                end
+            end
         end
 
         -- Send the explosion to the neighbouring tiles.
