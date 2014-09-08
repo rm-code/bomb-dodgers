965bea1e72d43287ea08db8842f9edeb2e327055
Robert Machmer <robert.machmer@gmail.com>
2014-09-08 22:16:41 +0200

    Bump version.

--- conf.lua
+++ conf.lua
@@ -8,7 +8,7 @@ local GAME_TITLE = "Bomb Dodgers";
 
 local LOVE_VERSION = "0.9.1";
 
-local GAME_VERSION = "0000";
+local GAME_VERSION = "v0086-3539";
 
 -- ------------------------------------------------
 -- Local variables
