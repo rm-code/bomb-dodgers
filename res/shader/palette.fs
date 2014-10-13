c2336218997bff445886443e615c1f71e822f623
Robert Machmer <robert.machmer@gmail.com>
2014-10-13 18:13:18 +0200

    Palette shader will take love.graphics.setColor into account.
    Before, it only was affected by the alpha channel of setColor. Now it
    also takes the red channel into account when deciding what color to
    assign to a fragment.

--- res/shader/palette.fs
+++ res/shader/palette.fs
@@ -12,7 +12,7 @@ vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords 
     vec4 tex = Texel(texture, texture_coords);
 
     // Get the color from the LUT based on the color of the texture.
-    vec4 lut = Texel(lut, vec2(tex.r, index / palettes));
+    vec4 lut = Texel(lut, vec2(tex.r * color.r, index / palettes));
 
     // Make sure the texture's alpha channel is affected by love.graphics.setColor(...).
     lut.a = tex.a *color.a;
