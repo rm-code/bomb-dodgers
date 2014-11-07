da7f8a355de5107070d8f4e7daa867eecf94e93a
Robert Machmer <robert.machmer@gmail.com>
2014-11-07 16:02:57 +0100

    Add game icon.

--- conf.lua
+++ conf.lua
@@ -48,7 +48,7 @@ function love.conf(t)
     t.console = true;
 
     t.window.title = GAME_TITLE;
-    t.window.icon = nil;
+    t.window.icon = 'res/img/icon.png';
     t.window.width = 640;
     t.window.height = 480;
     t.window.borderless = false;
