b54d0c429fba8f7d105add8a30eb111da9c61725
Robert Machmer <robert.machmer@gmail.com>
2016-02-01 11:03:09 +0100

    Replace code for checking hardware support

--- main.lua
+++ main.lua
@@ -44,37 +44,6 @@ Modal = require('src/screens/Modal');
 local info = false;
 
 -- ------------------------------------------------
--- Local functions
--- ------------------------------------------------
-
----
--- Check if the hardware supports certain features.
---
-local function checkSupport()
-    print("\n---- SUPPORTED ---- ");
-    print("Canvas:         " .. tostring(love.graphics.isSupported('canvas')));
-    print("PO2:            " .. tostring(love.graphics.isSupported('npot')));
-    print("Subtractive BM: " .. tostring(love.graphics.isSupported('subtractive')));
-    print("Shaders:        " .. tostring(love.graphics.isSupported('shader')));
-    print("HDR Canvas:     " .. tostring(love.graphics.isSupported('hdrcanvas')));
-    print("Multicanvas:    " .. tostring(love.graphics.isSupported('multicanvas')));
-    print("Mipmaps:        " .. tostring(love.graphics.isSupported('mipmap')));
-    print("DXT:            " .. tostring(love.graphics.isSupported('dxt')));
-    print("BC5:            " .. tostring(love.graphics.isSupported('bc5')));
-    print("SRGB:           " .. tostring(love.graphics.isSupported('srgb')));
-
-    print("\n---- RENDERER  ---- ");
-    local name, version, vendor, device = love.graphics.getRendererInfo()
-    print(string.format("Name: %s \nVersion: %s \nVendor: %s \nDevice: %s", name, version, vendor, device));
-
-    if not love.graphics.isSupported('shader') then
-        local profile = ProfileHandler.load();
-        profile.shaders = false;
-        ProfileHandler.save(profile);
-    end
-end
-
--- ------------------------------------------------
 -- Loading
 -- ------------------------------------------------
 
@@ -87,7 +56,9 @@ function love.load()
     print("===================")
 
     -- Check the user's hardware.
-    checkSupport();
+    print( "\n---- RENDERER  ---- " );
+    local name, version, vendor, device = love.graphics.getRendererInfo();
+    print( string.format( "Name: %s \nVersion: %s \nVendor: %s \nDevice: %s", name, version, vendor, device ));
 
     -- Set default filters.
     love.graphics.setDefaultFilter('nearest', 'nearest');
