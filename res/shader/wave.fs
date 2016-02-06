b44343938a285d95b705cd405e9f324fa14c1f21
Robert Machmer <robert.machmer@gmail.com>
2016-02-06 13:12:04 +0100

    Possible fix #2
    Renamed shader types to follow GLSL standards.

--- res/shader/wave.fs
+++ res/shader/wave.fs
@@ -20,12 +20,12 @@
 /*                                                                               */
 /*===============================================================================*/
 
-extern number time = 0.0;
-const number size = 32.0;
-const number strength = 8.0;
+uniform float time = 0.0;
+const float size = 32.0;
+const float strength = 8.0;
 const vec2 res = vec2(512.0, 512.0);
 
-vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 pixel_coords)
+vec4 effect(vec4 color, sampler2D texture, vec2 texture_coords, vec2 pixel_coords)
 {
     float tmp = sin(sqrt(pow(texture_coords.x * size - size / 2.0, 2.0) + pow(texture_coords.y * size - size / 2.0, 2.0)) - time * 16.0);
     vec2 uv = vec2(texture_coords.x - tmp * strength / 1024.0, texture_coords.y - tmp * strength / 1024.0);
